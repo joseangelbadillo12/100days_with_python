5caa8805658cd8dbce9413187bf1f29d1108a223
jose_badillo <jose.badillo12@gmail.com>
2024-05-13 14:39:49 -0600

    Completed final day 8 final project and added the art file

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -1,10 +1,8 @@
-# Part 1: Implement a Caesar cipher that shifts all letters by a fixed number of positions.
+from art import logo
+
+# Part 1: Implement a Caesar cipher that shifts all char by a fixed number of positions.
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'endcode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 # def encrypt(text, shift):
 #     cipher_text = ""
@@ -37,10 +35,29 @@ def caesar(text, shift, direction):
     answer = ""
     if direction == "decode":
         shift *= -1
-    for letters in text:
-        alphabet_index = alphabet.index(letters)
-        new_index = alphabet_index + shift
-        answer += alphabet[new_index]
+    for char in text:
+        if char in alphabet:
+            alphabet_index = alphabet.index(char)
+            new_index = alphabet_index + shift
+            answer += alphabet[new_index]
+        else:
+            answer += char    
     print(f"The {direction}d text is {answer}")
 
-caesar(text, shift, direction)
+print(logo)
+direction = input("Type 'endcode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+shift = shift % 26
+continue_cipher = False
+while not continue_cipher:
+    input_continue = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if input_continue == "no":
+        continue_cipher = True
+        print("Goodbye!")
+    else:
+        direction = input("Type 'endcode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        shift = shift % 26
+        caesar(text, shift, direction)
